ae6cde92513d3fa8cec808d9a77bbca8267d7388
niazmurshedsiam <niazmurshed16@gmail.com>
2021-03-30 09:59:08 +0600

    ema-john-server-with-server  done

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA7k1m6HNSO-xaKmhJn6mj38PB0QtWw0Vs",
+  authDomain: "auth-recap-9a3df.firebaseapp.com",
+  projectId: "auth-recap-9a3df",
+  storageBucket: "auth-recap-9a3df.appspot.com",
+  messagingSenderId: "862795973954",
+  appId: "1:862795973954:web:145c59fa15242be70534db"
+};
+export default firebaseConfig;
