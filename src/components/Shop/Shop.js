17c6104880fb845c7482c20e49660c1c0b8fdb30
niazmurshedsiam <niazmurshed16@gmail.com>
2021-04-01 00:34:18 +0600

    load some products using keys

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -21,15 +21,16 @@ const Shop = () => {
         const savedCart = getDatabaseCart();
         const productKeys = Object.keys(savedCart);
         console.log(productKeys,products);
-        if(products.length > 0){
-            const previousCart = productKeys.map( existingKey => {
-                const product = products.find( pd => pd.key === existingKey);
-                product.quantity = savedCart[existingKey];
-                return product;
-            } )
-            setCart(previousCart);
-        }
-    }, [products])
+        fetch('http://localhost:5000/productsByKeys',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(productKeys)
+        })
+        .then(res => res.json())
+        .then(data => setCart(data))
+    }, [])
 
     const handleAddProduct = (product) =>{
         const toBeAddedKey = product.key;
