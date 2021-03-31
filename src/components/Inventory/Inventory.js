fb70444d6effb87e7f13a976a5cafc9e7b5a05e4
niazmurshedsiam <niazmurshed16@gmail.com>
2021-04-01 02:13:45 +0600

    Save orders in a new data collection in mongodb

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import fakeData from '../../fakeData';
+import React from "react";
+// import fakeData from '../../fakeData';
 const Inventory = () => {
-    const handleAddProduct = () =>{
-        fetch('http://localhost:5000/addProduct', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(fakeData)
-        })
-    }
-    return (
-        <div>
-           <button onClick={handleAddProduct}>Add Product</button>
-        </div>
-    );
+  const handleAddProduct = () => {
+    const product = {};
+    fetch("http://localhost:5000/addProduct", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(product),
+    });
+  };
+  return (
+    <div>
+      <form action="">
+          <p><span>Name: </span><input type="text"/></p>
+          <p><span>Price: </span><input type="text"/></p>
+          <p><span>Quantity: </span><input type="text"/></p>
+          <p><span>Product Image: </span><input type="file"/></p>
+        <button onClick={handleAddProduct}>Add Product</button>
+      </form>
+    </div>
+  );
 };
 
 export default Inventory;
